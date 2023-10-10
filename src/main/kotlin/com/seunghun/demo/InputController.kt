1b0cec2c8d4aaf30f0a7f913dece8dada9969558
yabby <yabby1997@gmail.com>
2023-10-10 22:31:37 +0900

    Implement Post method

--- src/main/kotlin/com/seunghun/demo/InputController.kt
+++ src/main/kotlin/com/seunghun/demo/InputController.kt
@@ -1,8 +1,10 @@
 package com.seunghun.demo
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 
 @RestController
 class InputController {
@@ -10,4 +12,14 @@ class InputController {
     fun print(@PathVariable word: String) {
         println(word)
     }
+
+    @PostMapping("/data")
+    fun data(@RequestBody data: Data) {
+        println("POST Request Received: " + data.title + " " + data.value)
+    }
+}
+
+class Data {
+    var title: String? = null
+    var value: Int? = null
 }
